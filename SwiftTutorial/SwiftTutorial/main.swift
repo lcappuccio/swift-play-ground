1b494787e30f581919e1cb41dd2e11c2cdef9c81
Leonardo Cappuccio <leo@systemexception.org>
2015-04-15 22:45:48 +0200

    wip tutorial

--- SwiftTutorial/SwiftTutorial/main.swift
+++ SwiftTutorial/SwiftTutorial/main.swift
@@ -10,3 +10,13 @@ import Foundation
 
 println("Hello, World!")
 
+// SIMPLE VALUES
+var myVariable = 42
+// Cast to String
+println("Using var " + String(myVariable))
+myVariable = 50
+// Or use compact notation
+println("Clean  \(myVariable)")
+let myConstant = 42
+println("Constant \(myConstant)")
+
