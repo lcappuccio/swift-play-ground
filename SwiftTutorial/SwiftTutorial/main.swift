f5cc39a687793f1ed955dc8bd5c5a4ad467e944d
Leonardo Cappuccio <leo@systemexception.org>
2015-04-15 22:54:40 +0200

    wip tutorial

--- SwiftTutorial/SwiftTutorial/main.swift
+++ SwiftTutorial/SwiftTutorial/main.swift
@@ -20,3 +20,21 @@ println("Clean  \(myVariable)")
 let myConstant = 42
 println("Constant \(myConstant)")
 
+// Otherwise specify type explicitly
+let implicitInteger = 70
+println("Integer \(implicitInteger)")
+let implicitDouble = 10.0
+println("Integer \(implicitDouble)")
+let explicitDouble: Double = 70.5
+println("Integer \(explicitDouble)")
+
+// Create a Float constant
+let explicitFloat: Float = 4
+println("Float \(explicitFloat)")
+
+
+// Types are never implicitly converted, to do so convert explicitly
+let label = "The value is "
+let someValue = 111
+let myLabel = label + String(someValue)
+println(myLabel)
