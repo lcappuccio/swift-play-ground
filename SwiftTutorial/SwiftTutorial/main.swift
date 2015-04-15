c09f4023ddfb8c6f95624ca5ac123b84f2367216
Leonardo Cappuccio <leo@systemexception.org>
2015-04-15 22:33:15 +0200

    Workspace and Project creation

---
+++ SwiftTutorial/SwiftTutorial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTutorial
+//
+//  Created by Leo on 15/04/15.
+//  Copyright (c) 2015 Leonardo Cappuccio. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
